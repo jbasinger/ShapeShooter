5ac770467873d9b403cddb577536445dd170f2a2
Justin Basinger <justin.m.basinger@gmail.com>
2016-04-17 12:22:52 -0400

    New player sprites, triangle ship left and right sprites

---
+++ Assets/Scripts/ChangeSpriteOnMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSpriteOnMovement : MonoBehaviour {
+
+  public Sprite changeSprite;
+  public Sprite origSprite;
+
+  SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+    render = GetComponent<SpriteRenderer>();
+	}
+
+  void FixedUpdate() {
+
+    float x = Input.GetAxis("Horizontal");// * speed;
+
+    render.flipX = false;
+
+    if(x < 0) {
+      render.flipX = true;
+      render.sprite = changeSprite;
+    } else if (x == 0) {
+      render.sprite = origSprite;
+    } else {
+      render.sprite = changeSprite;
+    }
+
+  }
+}
